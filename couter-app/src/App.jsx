c94101ab635caae911459f004ea825cfef6fd712
Harshul <harshulmehra_se22a11_59@dtu.ac.in>
2025-04-04 16:43:13 +0530

    adding changes

--- couter-app/src/App.jsx
+++ couter-app/src/App.jsx
@@ -3,20 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-
 function App() {
   const [count, setCount] = useState(0); // defining the initial state
 
-  function onClickHandler() {
-    setCount(count + 1);
-  }
-
   return (
     <div>
-      <button onClick={onClickHandler}>Counter: {count} </button>
+      <CustomButton count = {count} setCount={setCount}></CustomButton>
+      <CustomButton count = {count+1}  setCount={setCount}></CustomButton>
     </div> 
   )
+}
+
+function CustomButton(props) {
+
+  function onClickHandler() {
+    props.setCount(props.count + 1);
+  }
+
+  return <button onClick={onClickHandler}>
+    Counter {props.count}
+  </button> 
 }
 
 export default App
